397db2401fa6695c086684eefd5614f41928ed22
wangzhenyao90-ship-it <wangzhenyao90@gmail.com>
2026-06-06 20:40:34 +0800

    Delete starwing-tech-website-final/starwing-tech-website-final directory

--- starwing-tech-website-final/starwing-tech-website-final/src/components/Footer.js
+++
@@ -1,8 +0,0 @@
-function Footer(content) {
-  return `
-    <footer class="footer">
-      <p>${content.footer.copyright}</p>
-      <a href="#hero">${content.footer.backToTop}</a>
-    </footer>
-  `;
-}
